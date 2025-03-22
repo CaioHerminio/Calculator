b2b167d7dc6623e921c8010861bb2fa357ec7854
Caio Hermínio <114634602+CaioHerminio@users.noreply.github.com>
2025-03-22 00:07:45 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+let currentInput = '';
+
+function appendNumber(number) {
+  currentInput += number;
+  document.getElementById('display').value = currentInput;
+}
+
+function appendOperator(operator) {
+  if (currentInput !== '') {
+    currentInput += ` ${operator} `;
+    document.getElementById('display').value = currentInput;
+  }
+}
+
+function clearDisplay() {
+  currentInput = '';
+  document.getElementById('display').value = currentInput;
+}
+
+function calculateResult() {
+  try {
+    currentInput = eval(currentInput).toString();
+    document.getElementById('display').value = currentInput;
+  } catch (e) {
+    document.getElementById('display').value = 'Error';
+    currentInput = '';
+  }
+}
